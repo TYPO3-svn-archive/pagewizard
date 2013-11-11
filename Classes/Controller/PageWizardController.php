e50d91a6ae9de452a27072d064f0241b6c6a0be0
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-11 10:28:17 +0000

    [FEATURE] Complete rewrite in Extbase / Fluid
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pagewizard/trunk@81406 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Controller/PageWizardController.php
@@ -0,0 +1,262 @@
+<?php
+/*****************************************************************************
+ *  Copyright notice
+ *
+ *  ⓒ 2013 Michiel Roos <michiel@maxserv.nl>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is free
+ *  software; you can redistribute it and/or modify it under the terms of the
+ *  GNU General Public License as published by the Free Software Foundation;
+ *  either version 2 of the License, or (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ****************************************************************************/
+/**
+ * Created by PhpStorm.
+ * Author: Michiel Roos <michiel@maxserv.nl>
+ * Date: 06/11/13
+ * Time: 10:16
+ */
+
+/**
+ * PageWizard controller
+ *
+ * @author Michiel Roos <michiel@maxserv.nl>
+ * @package TYPO3
+ * @subpackage pagewizard
+ */
+class Tx_Pagewizard_Controller_PageWizardController extends
+	Tx_Extbase_MVC_Controller_ActionController {
+
+	/**
+	 * @var Tx_Pagewizard_Domain_Repository_PageRepository
+	 */
+	protected $pageRepository;
+
+	/**
+	 * inject the pageRepository
+	 *
+	 * @param Tx_Pagewizard_Domain_Repository_PageRepository $pageRepository
+	 *
+	 * @return void
+	 */
+	public function injectPageRepository(Tx_Pagewizard_Domain_Repository_PageRepository $pageRepository) {
+		$this->pageRepository = $pageRepository;
+	}
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+			/** @var t3lib_positionMap $positionMap */
+		$positionMap = t3lib_div::makeInstance('t3lib_positionMap');
+
+			// Feed the storagePid to the fluid template so we can give the user
+			// some feedback when it has not yet been set.
+		$configuration = $this->configurationManager->getConfiguration(
+			Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK
+		);
+		$values['storagePid'] = $configuration['persistence']['storagePid'];
+
+		$get = t3lib_div::_GET();
+		$id = intval($get['id']);
+		$values['positionPid'] = intval($get['positionPid']);
+			// Can only be: crPage, hardcoded in: class.t3lib_positionmap.php
+		$values['command'] = $get['cmd'];
+
+		switch ($values['command']) {
+			case 'crPage':
+					// Show page tree templates
+				$pages = $this->pageRepository->findAll();
+
+					// Initialize tree object:
+					/** @var t3lib_browsetree $tree */
+				$tree = t3lib_div::makeInstance('t3lib_browsetree');
+					// Also store tree prefix markup:
+				$tree->makeHTML = 2;
+				$tree->init();
+				$tree->ext_IconMode = TRUE;
+				$tree->ext_showPageId = $GLOBALS['BE_USER']->getTSConfigVal('options.pageTree.showPageIdWithTitle');
+				$tree->showDefaultTitleAttribute = TRUE;
+				$tree->thisScript = t3lib_div::getIndpEnv('REQUEST_URI');
+
+					/** @var Tx_Pagewizard_Domain_Model_Page $page */
+				foreach ($pages as $page) {
+					$uid = $page->getUid();
+					$values['pageTemplates'][$uid]['page'] = $page;
+
+						// Set starting page id of the tree (overrides webmounts):
+					if ($uid > 0) {
+						$tree->MOUNTS = array(0 => $uid);
+					}
+					$tree->setTreeName('pagewizard_' . $uid);
+
+						// Creating top icon; the current page
+					$tree->getBrowsableTree();
+
+					$values['pageTemplates'][$uid]['tree'] = $this->processExpandCollapseLinks($tree->printTree(), $uid);
+				}
+
+				break;
+			default:
+					// Show position chooser
+				$pageRecord = t3lib_BEfunc::readPageAccess($id, $GLOBALS['BE_USER']->getPagePermsClause(1));
+
+				$values['positionMap'] = $positionMap->positionTree(
+					t3lib_div::_GET('id'),
+					$pageRecord,
+					$GLOBALS['BE_USER']->getPagePermsClause($id),
+					t3lib_div::getIndpEnv('REQUEST_URI')
+				);
+		}
+		$this->view->assignMultiple($values);
+	}
+
+	/**
+	 * Create action
+	 *
+	 * @return void
+	 */
+	public function createAction() {
+		$arguments = $this->request->getArguments();
+
+			// This part is modeled after alt_doc.php
+
+			/** @var t3lib_TCEmain $tce */
+		$tce = t3lib_div::makeInstance('t3lib_TCEmain');
+		$tce->start('', '', $GLOBALS['BE_USER']);
+		$tce->stripslashes_values = 0;
+
+			// Update the page tree after the copy operation
+		t3lib_BEfunc::setUpdateSignal('updatePageTree');
+
+		if ($arguments['copyMode'] === 'pagesOnly') {
+			$copyTablesArray = array('pages');
+		} else {
+				// These are the tables, the user may modify
+			$copyTablesArray = $GLOBALS['BE_USER']->user['admin'] ?
+				$tce->compileAdminTables() :
+				explode(',', $GLOBALS['BE_USER']->groupData['tables_modify']);
+			$copyTablesArray = array_unique($copyTablesArray);
+		}
+
+			// Setting default values specific for the user:
+		$TCAdefaultOverride = $GLOBALS['BE_USER']->getTSConfigProp('TCAdefaults');
+		if (is_array($TCAdefaultOverride)) {
+			$tce->setDefaultsFromUserTS($TCAdefaultOverride);
+		}
+
+			// Setting internal vars:
+		if ($GLOBALS['BE_USER']->uc['neverHideAtCopy']) {
+			$tce->neverHideAtCopy = 1;
+		}
+
+		$tce->copyTree = t3lib_utility_Math::forceIntegerInRange($GLOBALS['BE_USER']->uc['copyLevels'], 0, 100);
+
+			// Copy this page we're on. And set first-flag (this will trigger that
+			// the record is hidden if that is configured)!
+		$theNewRootId = $tce->copySpecificPage($arguments['templateId'], $arguments['positionPid'], $copyTablesArray);
+
+			// If we're going to copy recursively...:
+		if ($theNewRootId && $tce->copyTree) {
+
+				// Get ALL subpages to copy (read-permissions are respected!):
+			$CPtable = $tce->int_pageTreeInfo(Array(), $arguments['templateId'], $tce->copyTree, $theNewRootId);
+
+				// Now copying the subpages:
+			foreach ($CPtable as $thePageUid => $thePagePid) {
+				$newPid = $tce->copyMappingArray['pages'][$thePagePid];
+				if (isset($newPid)) {
+					$tce->copySpecificPage($thePageUid, $newPid, $copyTablesArray);
+				} else {
+					$tce->log('pages', $arguments['templateId'], 5, 0, 1, 'Something went wrong during copying branch');
+					break;
+				}
+			}
+		} // else the page was not copied. Too bad...
+
+			// Checking referer / executing
+		$refInfo = parse_url(t3lib_div::getIndpEnv('HTTP_REFERER'));
+		$httpHost = t3lib_div::getIndpEnv('TYPO3_HOST_ONLY');
+		if ($httpHost != $refInfo['host'] && !$GLOBALS['TYPO3_CONF_VARS']['SYS']['doNotCheckReferer']) {
+			$tce->log('', 0, 0, 0, 1, "Referer host '%s' and server host '%s' did not match and veriCode was not valid either!", 1, array($refInfo['host'], $httpHost));
+			debug('Error: Referer host did not match with server host.');
+		} else {
+
+				// Perform the saving operation with TCEmain:
+			$tce->process_uploads($_FILES);
+			$tce->process_datamap();
+			$tce->process_cmdmap();
+		}
+
+			// Directly jump to edit mode of the created page
+		$uri = '/typo3/alt_doc.php?returnUrl=' .
+			urlencode('/typo3/sysext/cms/layout/db_layout.php?id=' . $theNewRootId) .
+			'&edit[pages][' . $theNewRootId . ']=edit';
+		$this->redirectToUri($uri);
+	}
+
+	/**
+	 * Processes the expand/collapse links. Rewrite ?PM to &PM
+	 *
+	 * @param string  $content Content to be processed
+	 * @param integer $uid The id of the page
+	 *
+	 * @return string The processed and modified content
+	 */
+	protected function processExpandCollapseLinks($content, $uid) {
+		if (strpos($content, '?PM=') !== FALSE && $uid > 0) {
+			$content = preg_replace('/\?PM/', '&PM', $content);
+		}
+		return $content;
+	}
+}
+
+/**
+ * Extension for the tree class that generates the tree of pages in the
+ * page-wizard mode
+ *
+ * @author Michiel Roos <michiel@maxserv.nl>
+ * @package TYPO3
+ * @subpackage pagewizard
+ */
+class localPageTree extends t3lib_pageTree {
+
+	/**
+	 * Returns the value for the image "title" attribute
+	 *
+	 * @param array $row The input row array (where the key "title" is used for the title)
+	 *
+	 * @return string The attribute value (is htmlspecialchared() already)
+	 * @see wrapIcon()
+	 */
+	function getTitleAttrib($row) {
+		return 'uid ' . $row['uid'] . ': ' . htmlspecialchars($row['title']);
+	}
+
+	/**
+	 * Determines whether to expand a branch or not.
+	 * Here the branch is expanded if the current id matches the global id for
+	 * the listing/new
+	 *
+	 * @param integer $id The ID (page id) of the element
+	 *
+	 * @return boolean Returns TRUE if the IDs matches
+	 */
+	function expandNext($id) {
+		return $id == $GLOBALS['SOBE']->id ? 1 : 0;
+	}
+}
+
+?>
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "pagewizard"
-#
-# Auto generated 13-12-2008 09:13
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pagewizard".
+ *
+ * Auto generated 09-11-2013 20:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Page Wizard',
-	'description' => 'Create new page from predefined preset pages',
+	'title' => 'Page Tree Wizard',
+	'description' => 'Create new page tree from a set of template page trees',
 	'category' => 'be',
 	'shy' => 0,
 	'version' => '1.0.6',
@@ -20,16 +20,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'module' => 'cm1',
-	'state' => 'stable',
+	'module' => '',
+	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'Dev-Team Typoheads',
-	'author_email' => 'dev@typoheads.at',
-	'author_company' => 'Typoheads GmbH',
+	'author' => 'Michiel Roos, Dev-Team Typoheads, Reinhard Führicht',
+	'author_email' => 'extensions@maxserv.nl',
+	'author_company' => 'MaxServ',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"067b";s:10:"README.txt";s:4:"ee2d";s:9:"Thumbs.db";s:4:"e9b1";s:24:"class.t3lib_tceforms.php";s:4:"36e0";s:27:"class.tx_pagewizard_cm1.php";s:4:"d04e";s:12:"ext_icon.gif";s:4:"1b05";s:14:"ext_tables.php";s:4:"1517";s:13:"locallang.xml";s:4:"325a";s:13:"cm1/Thumbs.db";s:4:"d884";s:25:"cm1/class.positionmap.php";s:4:"2c16";s:13:"cm1/clear.gif";s:4:"cc11";s:15:"cm1/cm_icon.gif";s:4:"591c";s:12:"cm1/conf.php";s:4:"0970";s:13:"cm1/index.php";s:4:"958a";s:17:"cm1/locallang.xml";s:4:"433b";s:13:"doc/Thumbs.db";s:4:"39cb";s:14:"doc/manual.sxw";s:4:"036c";s:19:"doc/wizard_form.dat";s:4:"497b";s:20:"doc/wizard_form.html";s:4:"addb";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"53b0";s:16:"ext_autoload.php";s:4:"a1db";s:12:"ext_icon.gif";s:4:"1b05";s:14:"ext_tables.php";s:4:"4812";s:24:"ext_typoscript_setup.txt";s:4:"6221";s:43:"Classes/Controller/PageWizardController.php";s:4:"e882";s:29:"Classes/Domain/Model/Page.php";s:4:"73bc";s:44:"Classes/Domain/Repository/PageRepository.php";s:4:"f170";s:49:"Resources/Private/Backend/Layouts/PageWizard.html";s:4:"46ae";s:58:"Resources/Private/Backend/Templates/PageWizard/Create.html";s:4:"e74f";s:57:"Resources/Private/Backend/Templates/PageWizard/Index.html";s:4:"8c0a";s:40:"Resources/Private/Language/locallang.xml";s:4:"1ba9";s:41:"Resources/Public/Images/clickMenuIcon.gif";s:4:"591c";s:46:"Resources/Public/StyleSheets/Backend/Style.css";s:4:"c5a9";s:14:"doc/manual.sxw";s:4:"c294";}',
+	'suggests' => array(
+	),
 );
 
 ?>
